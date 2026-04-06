5f430b33cf3e59ca007b5449edfcb86c936ed251
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-06 23:53:59 +0600

    bg imgae set 2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import bgImg from "../assest/images/Rectangle 1.png"
-import fbIcon from "../assest/icons/fb.png"
+// import bgImg from "../assest/images/Rectangle 1.png"
+// import fbIcon from "../assest/icons/fb.png"
 const Home = () => {
     return (
         <div>
-            <h1>Home Pages Here</h1>
-            <h1>Home Pages Here</h1>
-            {/* <img src={bgImg} alt="" />
-            <img src={fbIcon} alt="" /> */}
+          
         </div>
     );
 };
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -5,12 +5,14 @@ import bgImage from '../assest/images/Rectangle 1.png'
 
 const Root = () => {
     return (
-        <div  style={{backgroundImage:`url(${bgImage})`}} >
-          
-           <div>
-             <Navbar></Navbar>
-            <Outlet></Outlet>
-           </div>
+        <div
+            className="min-h-screen bg-cover bg-center"
+            style={{ backgroundImage: `url(${bgImage})` }}
+        >
+            <div className="min-h-screen bg-black/40">
+                <Navbar />
+                <Outlet />
+            </div>
         </div>
     );
 };
