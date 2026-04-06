08371e7cffcb24961f4922b31a1edc24fc6d90a4
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-06 10:01:02 +0600

    home page design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import bgImg from "../assest/images/Rectangle 1.png"
+import fbIcon from "../assest/icons/fb.png"
 const Home = () => {
     return (
         <div>
             <h1>Home Pages Here</h1>
+            <img src={bgImg} alt="" />
+            <img src={fbIcon} alt="" />
         </div>
     );
 };
