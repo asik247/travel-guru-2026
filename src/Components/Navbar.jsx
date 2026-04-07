678efaf9d8da7bc9ad8bc99a5de6adb478131b6b
Asik <mdasikur5893@gmail.com>
2026-04-07 12:46:21 +0600

    aleart show

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
       <li><NavLink to={'contact'}>Contact</NavLink></li>
       <li><NavLink to={'login'}>Login</NavLink></li>
       <li><NavLink to={'login'}>SignUp</NavLink></li>
+    
     </>
   );
 
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Blog = () => {
+    alert("Hi")
     return (
         <div>
             <h3>Blog Here</h3>
             <h3>Blog Here</h3>
             <h3>Blog Here</h3>
             <h3>Blog Here</h3>
+            
         </div>
     );
 };
