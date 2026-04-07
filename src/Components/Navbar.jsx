cc196ea4f503dea6dcb167c5a03162acb0defcc0
Asik <mdasikur5893@gmail.com>
2026-04-07 12:36:07 +0600

    x

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
       <li><NavLink to={'blog'}>Blog</NavLink></li>
       <li><NavLink to={'contact'}>Contact</NavLink></li>
       <li><NavLink to={'login'}>Login</NavLink></li>
+      <li><NavLink to={'login'}>SignUp</NavLink></li>
     </>
   );
 
